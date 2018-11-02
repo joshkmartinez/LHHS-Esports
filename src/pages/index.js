b13c71d605a49bff910428497f2922aceb9dd32d
Joshua K. Martinez <joshkmartinez@gmail.com>
2018-11-01 22:38:56 -0700

    what happened?

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,6 +88,7 @@ function ButtonAppBar(props) {
               /></div>
           </Toolbar>
         </AppBar>
+        
         <div display="inline"
           float="none">
           <iframe styles={{ width: 123 }} src="https://calendar.google.com/calendar/embed?src=42754uki8qmtnfuva13rrk44vs%40group.calendar.google.com&ctz=America%2FLos_Angeles" width="800" height="600" frameborder="0" scrolling="no"></iframe>
